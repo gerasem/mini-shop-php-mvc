7a0281241578be754abb351a6e7a001d9b35451f
gerasem <sougift@gmail.com>
2021-07-24 23:40:01 +0200

    bulma installed

--- config/routes.php
+++ config/routes.php
@@ -2,6 +2,9 @@
 return [
     'admin' => 'admin/admin/index',
     'admin/login' => 'admin/admin/login',
-    '' => '/frontend/index',
+
     'index.php' => '/frontend/index',
+    'index' => '/frontend/index',
+    'index.html' => '/frontend/index',
+    '' => '/frontend/index',
 ];
